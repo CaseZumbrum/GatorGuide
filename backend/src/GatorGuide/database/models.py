0558cbfee5eb68185166ff4a2761d30df8e8b647
Case Zumbrum <casezumbrum@gmail.com>
2025-02-12 15:39:02 -0500

    add doc strings to all models

--- backend/src/GatorGuide/database/models.py
+++ backend/src/GatorGuide/database/models.py
@@ -2,6 +2,8 @@ from sqlmodel import Field, Relationship, SQLModel
 
 
 class MajorCriticalTrackingLink(SQLModel, table=True):
+    """Link table between majors and their critical tracking courses"""
+
     course_id: int | None = Field(
         default=None, foreign_key="course.id", primary_key=True
     )
@@ -9,6 +11,8 @@ class MajorCriticalTrackingLink(SQLModel, table=True):
 
 
 class PrerequisiteGroupCourseLink(SQLModel, table=True):
+    """Link table between PrerequisiteGroup and their Courses"""
+
     prereq_group_id: int | None = Field(
         default=None, foreign_key="prequisitegroup.id", primary_key=True
     )
@@ -18,6 +22,8 @@ class PrerequisiteGroupCourseLink(SQLModel, table=True):
 
 
 class CoursePrerequisiteGroupLink(SQLModel, table=True):
+    """Link table between Courses and their PrerequisiteGroups"""
+
     course_id: int | None = Field(
         default=None, foreign_key="course.id", primary_key=True
     )
@@ -27,6 +33,8 @@ class CoursePrerequisiteGroupLink(SQLModel, table=True):
 
 
 class MajorRequiredLink(SQLModel, table=True):
+    """Link table between Majors and their Required Courses"""
+
     course_id: int | None = Field(
         default=None, foreign_key="course.id", primary_key=True
     )
@@ -34,6 +42,8 @@ class MajorRequiredLink(SQLModel, table=True):
 
 
 class RequirementGroupsCourseLink(SQLModel, table=True):
+    """Link table between RequirementGroups and their Courses"""
+
     course_id: int | None = Field(
         default=None, foreign_key="course.id", primary_key=True
     )
@@ -43,6 +53,8 @@ class RequirementGroupsCourseLink(SQLModel, table=True):
 
 
 class MajorRequiredGroupLink(SQLModel, table=True):
+    """Link table between Majors and their RequirementGroups"""
+
     Major_id: int | None = Field(default=None, foreign_key="major.id", primary_key=True)
     group_id: int | None = Field(
         default=None, foreign_key="requiredgroup.id", primary_key=True
@@ -50,6 +62,17 @@ class MajorRequiredGroupLink(SQLModel, table=True):
 
 
 class Course(SQLModel, table=True):
+    """Object representing a single course
+
+    Args:
+        id (int, optional): Primary key of the table, do NOT pass this parameter in unless you are aware of the affect it will have. Defaults to None.
+        code (str): Course code, should follow the UF course code format (ex: CEN3031)
+        name (str): Course name, allows spaces and special characters
+        description (str): Course description
+        credits (int): number of credits the course is worth, if a course can be worth different amounts of credits, create several courses for it
+        prerequisits (list[PrerequisitGroup]): prerequisits for the course
+    """
+
     id: int | None = Field(default=None, primary_key=True)
     code: str = Field(index=True)
     name: str
@@ -61,11 +84,28 @@ class Course(SQLModel, table=True):
 
 
 class PrequisiteGroup(SQLModel, table=True):
+    """Group used to store prerequisits for courses
+
+    Args:
+        id (int, optional): Primary key of the table, do NOT pass this parameter in unless you are aware of the affect it will have. Defaults to None.
+        Courses (list[Course]): courses within the prerequisite group
+    """
+
     id: int | None = Field(default=None, primary_key=True)
     courses: list[Course] = Relationship(link_model=PrerequisiteGroupCourseLink)
 
 
 class Major(SQLModel, table=True):
+    """Object representing a single major
+
+    Args:
+        id (int, optional): Primary key of the table, do NOT pass this parameter in unless you are aware of the affect it will have. Defaults to None.
+        name (str): Major name, allows spaces and special characters
+        critical_tracking (list[Course]): list of all critical tracking courses
+        required (list[Course]): list of all required (non-critical tracking) courses
+        groups (list[RequiredGroup]): list of all groups of requirements (ex: tech electives)
+    """
+
     id: int | None = Field(default=None, primary_key=True)
     name: str
     critical_tracking: list[Course] = Relationship(link_model=MajorCriticalTrackingLink)
@@ -73,6 +113,7 @@ class Major(SQLModel, table=True):
     groups: list["RequiredGroup"] = Relationship(link_model=MajorRequiredGroupLink)
 
     def print(self):
+        """print data on the major"""
         print("-------------------")
         print(f"Name: {self.name}")
         print("-------------------")
@@ -92,6 +133,15 @@ class Major(SQLModel, table=True):
 
 
 class RequiredGroup(SQLModel, table=True):
+    """Object representing a group of required courses
+
+    Args:
+        id (int, optional): Primary key of the table, do NOT pass this parameter in unless you are aware of the affect it will have. Defaults to None.
+        name (str): Group name, allows spaces and special characters (ex: tech electives)
+        credits (int): number of credits this group requires
+        courses (list[courses]): courses included in this group
+    """
+
     id: int | None = Field(default=None, primary_key=True)
     name: str
     credits: int
