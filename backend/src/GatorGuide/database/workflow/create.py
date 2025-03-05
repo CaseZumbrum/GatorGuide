c16c917dcae2c830e8aaf9fca75db648aebfb4cd
casezumbrum <casezumbrum@ufl.edu>
2025-03-05 12:34:56 -0500

    fix session setup

--- backend/src/GatorGuide/database/workflow/create.py
+++ backend/src/GatorGuide/database/workflow/create.py
@@ -6,20 +6,22 @@ import os
 from GatorGuide.database import models
 
 # path of the DB
-sqlite_file_name = pathlib.Path(__file__).parent.resolve().joinpath("../database.db")
-
-# If the DB already exists, delete it
-if os.path.isfile(sqlite_file_name):
-    os.remove(sqlite_file_name)
-
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-engine = create_engine(sqlite_url)
 
 
-def create_db_and_tables():
+def create_db_and_tables(sqlite_file_name):
+    # If the DB already exists, delete it
+    if os.path.isfile(sqlite_file_name):
+        os.remove(sqlite_file_name)
+
+    sqlite_url = f"sqlite:///{sqlite_file_name}"
+    engine = create_engine(sqlite_url)
     """Create the DB and the tables"""
     SQLModel.metadata.create_all(engine)
+    engine.dispose()
 
 
 if __name__ == "__main__":
-    create_db_and_tables()
+    sqlite_file_name = (
+        pathlib.Path(__file__).parent.resolve().joinpath("../database.db")
+    )
+    create_db_and_tables(sqlite_file_name)
