a62de97aeb1984d9dcc706844ae318534582ec27
Case Zumbrum <51789445+CaseZumbrum@users.noreply.github.com>
2025-03-13 17:21:21 -0400

    Update main.py

--- backend/src/GatorGuide/api/main.py
+++ backend/src/GatorGuide/api/main.py
@@ -39,13 +39,13 @@ app.openapi_schema = custom_openapi()
 # Commented this out because API Docs weren't starting, I dont think frontend/dist is a valid path? I will try to remember to uncomment
 # this before commit, but in case I forget I am leaving this comment here. - Louis
 # mount frontend
-# app.mount(
-#     "/",
-#     StaticFiles(
-#         directory=pathlib.Path(__file__)
-#         .parent.resolve()
-#         .joinpath("../../../../frontend/dist"),
-#         html=True,
-#     ),
-#     name="site",
-# )
+app.mount(
+    "/",
+    StaticFiles(
+        directory=pathlib.Path(__file__)
+        .parent.resolve()
+        .joinpath("../../../../frontend/dist"),
+        html=True,
+    ),
+    name="site",
+)
