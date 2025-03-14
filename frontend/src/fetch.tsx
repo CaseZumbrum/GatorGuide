0e9ac09b95d9a6aa4cf81432ad7bd8c0b0413a41
Case Zumbrum <casezumbrum@gmail.com>
2025-03-14 09:54:09 -0400

    add fetch demo

---
+++ frontend/src/fetch.tsx
@@ -0,0 +1,15 @@
+fetch(import.meta.env.VITE_API_HOST + "/courses", {
+  credentials: 'include',
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+}).then((response) => {
+  if (response.status == 200) {
+    response.json().then((courses) => {
+      console.log(courses)
+    });
+  }
+
+});
