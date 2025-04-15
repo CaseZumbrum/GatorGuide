681dd41a527c5e710c52d115fb4aae09fbc424dd
casezumbrum <casezumbrum@ufl.edu>
2025-04-15 00:35:18 -0400

    user login logic

--- frontend/src/LoginPage.tsx
+++ frontend/src/LoginPage.tsx
@@ -1,22 +1,62 @@
-fetch(import.meta.env.VITE_API_HOST + "/courses", {
-    credentials: 'include',
-    method: 'GET',
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-    }
-}).then((response) => {
-    if (response.status == 200) {
-        response.json().then((courses) => {
-            console.log(courses)
-        });
-    }
-
-});
+import { useEffect, useState } from "react";
+import { login, get_user_data, create_user } from "./Logic/login";
 
 function LoginPage() {
+  const [userName, setUserName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-    return ()
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        defaultValue={"email"}
+      ></input>
+
+      <input
+        type="text"
+        onChange={(e) => {
+          setUserName(e.target.value);
+        }}
+        defaultValue={"username"}
+      ></input>
+
+      <input
+        type="password"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        defaultValue={"password"}
+      ></input>
+
+      <button
+        onClick={(e) => {
+          login(userName, password);
+        }}
+      >
+        login
+      </button>
+      <button
+        onClick={(e) => {
+          create_user({ name: userName, email: email, plans: [] }, password);
+        }}
+      >
+        create_user
+      </button>
+      <button
+        onClick={(e) => {
+          get_user_data().then((user) => {
+            alert(JSON.stringify(user));
+          });
+        }}
+      >
+        get user data (make sure to sign in first)
+      </button>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
--- frontend/src/Types/User.ts
+++ frontend/src/Types/User.ts
@@ -1,7 +1,9 @@
 import FourYearPlan from "./FourYearPlan";
 
-interface User{
-    name: string,
-    email: string,
-    plans: FourYearPlan[]
+interface User {
+  name: string;
+  email: string;
+  plans: FourYearPlan[];
 }
+
+export default User;
